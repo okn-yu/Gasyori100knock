6485c612b7d8fd813532a384ce123ff2742813b5
okn-yu <haruka.koishikawa@gmail.com>
2019-06-02 12:44:43 +0900

    updated.

---
+++ Question_01_10/My_Answers/answer1.py
@@ -0,0 +1,26 @@
+import cv2
+
+img = cv2.imread("imori.jpg")
+
+#print(img.shape) # (128, 128, 3)
+#print(img[0][0]) # [132 80 67] = (B, G ,R)
+
+# All Black img.
+# img[:, :, (0, 1, 2)] = 0
+
+# All White img.
+#img[:, :, (0, 1, 2)] = 255
+
+b = img[:, :, 0].copy()
+g = img[:, :, 1].copy()
+r = img[:, :, 2].copy()
+
+img[:, :, 0] = r
+img[:, :, 1] = g
+img[:, :, 2] = b
+
+cv2.imshow('image', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+## 一言メモ
