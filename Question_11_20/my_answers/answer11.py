722cc416ff6e72cb884bc130b8997122786ba7e1
okn-yu <haruka.koishikawa@gmail.com>
2019-06-10 22:16:51 +0900

    updated.

---
+++ Question_11_20/my_answers/answer11.py
@@ -0,0 +1,25 @@
+import cv2
+import numpy as np
+
+img = cv2.imread("imori.jpg")
+H, W, C = img.shape
+
+# Kernel_size
+K_size = 3
+pad = K_size // 2
+
+out = np.zeros((H + 2 * pad, W + 2 * pad, C))
+out[pad:pad + H, pad:pad + W] = img.copy()
+
+for y in range(pad, pad + W):
+    for x in range(pad, pad + H):
+        for c in range(C):
+            out[y, x, c] = np.average(out[y - pad:y + pad + 1, x - pad:x + pad + 1, c])
+
+cv2.imshow('image', out[pad:pad + H, pad:pad + W].astype(np.uint8))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# 一言
+# np.medianは中央値
+# np.mean, np.averageは平均値
